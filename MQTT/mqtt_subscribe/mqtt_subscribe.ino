dc3287c1197a7e8d960618c45478bd56d3f1d437
Hanz <115916883+hanzlr@users.noreply.github.com>
2026-06-28 22:58:49 +0700

    Implement MQTT subscriber with WiFi connection

--- MQTT/mqtt_subscribe/mqtt_subscribe.ino
+++ MQTT/mqtt_subscribe/mqtt_subscribe.ino
@@ -1,1 +1,57 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
 
+const char* ssid        = "YOUR_SSID";
+const char* password    = "YOUR_PASSWORD";
+const char* mqtt_server = "broker.hivemq.com";
+const int   mqtt_port   = 1883;
+const char* topic       = "nexalab/test";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message received [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void connectWiFi() {
+  Serial.print("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" Connected!");
+}
+
+void connectMQTT() {
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT...");
+    if (client.connect("ESP32Subscriber")) {
+      Serial.println(" Connected!");
+      client.subscribe(topic);
+    } else {
+      Serial.print(" Failed, rc=");
+      Serial.println(client.state());
+      delay(2000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  connectWiFi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) connectMQTT();
+  client.loop();
+}
