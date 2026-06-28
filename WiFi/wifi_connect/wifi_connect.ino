cbdcf3f73f9b4cd17d24febb879032a1d447942f
Hanz <115916883+hanzlr@users.noreply.github.com>
2026-06-28 22:56:47 +0700

    Add WiFi connection setup in wifi_connect.ino

--- WiFi/wifi_connect/wifi_connect.ino
+++ WiFi/wifi_connect/wifi_connect.ino
@@ -1,1 +1,25 @@
+#include <WiFi.h>
 
+const char* ssid     = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Connecting to WiFi...");
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Nothing here
+}
