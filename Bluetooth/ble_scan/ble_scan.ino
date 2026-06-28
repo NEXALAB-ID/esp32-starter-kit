353345c74571ca16bbac2aa156dda5bc54ce885e
Hanz <115916883+hanzlr@users.noreply.github.com>
2026-06-28 22:59:46 +0700

    Add BLE scanning functionality
    Implement a BLE scanner that detects and displays nearby devices.

--- Bluetooth/ble_scan/ble_scan.ino
+++ Bluetooth/ble_scan/ble_scan.ino
@@ -1,1 +1,37 @@
+#include <BLEDevice.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
 
+BLEScan* pBLEScan;
+int scanTime = 5;
+
+class AdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    Serial.print("Device found: ");
+    Serial.print(advertisedDevice.getName().c_str());
+    Serial.print(" | RSSI: ");
+    Serial.println(advertisedDevice.getRSSI());
+  }
+};
+
+void setup() {
+  Serial.begin(115200);
+  BLEDevice::init("");
+
+  pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true);
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);
+
+  Serial.println("BLE Scanner Ready");
+}
+
+void loop() {
+  Serial.println("Scanning...");
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  Serial.print("Devices found: ");
+  Serial.println(foundDevices.getCount());
+  pBLEScan->clearResults();
+  delay(5000);
+}
