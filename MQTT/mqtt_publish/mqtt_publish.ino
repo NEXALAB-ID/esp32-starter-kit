c2c0c04414c133cb6377dc718bd5368fcd3d239c
Hanz <115916883+hanzlr@users.noreply.github.com>
2026-06-28 22:58:31 +0700

    Add MQTT publish functionality to ESP32

--- MQTT/mqtt_publish/mqtt_publish.ino
+++ MQTT/mqtt_publish/mqtt_publish.ino
@@ -1,1 +1,51 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
 
+const char* ssid       = "YOUR_SSID";
+const char* password   = "YOUR_PASSWORD";
+const char* mqtt_server = "broker.hivemq.com";
+const int   mqtt_port   = 1883;
+const char* topic       = "nexalab/test";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void connectWiFi() {
+  Serial.print("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" Connected!");
+}
+
+void connectMQTT() {
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT...");
+    if (client.connect("ESP32Client")) {
+      Serial.println(" Connected!");
+    } else {
+      Serial.print(" Failed, rc=");
+      Serial.println(client.state());
+      delay(2000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  connectWiFi();
+  client.setServer(mqtt_server, mqtt_port);
+}
+
+void loop() {
+  if (!client.connected()) connectMQTT();
+  client.loop();
+
+  String message = "Hello from ESP32!";
+  client.publish(topic, message.c_str());
+  Serial.println("Message published: " + message);
+
+  delay(5000);
+}
