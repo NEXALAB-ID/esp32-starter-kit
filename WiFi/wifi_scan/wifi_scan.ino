b5824980d8783291f05acb64fdb6a3c7e18fa3a0
Hanz <115916883+hanzlr@users.noreply.github.com>
2026-06-28 22:57:09 +0700

    Implement WiFi scanning functionality

--- WiFi/wifi_scan/wifi_scan.ino
+++ WiFi/wifi_scan/wifi_scan.ino
@@ -1,1 +1,34 @@
+#include <WiFi.h>
 
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  Serial.println("WiFi Scanner Ready");
+}
+
+void loop() {
+  Serial.println("Scanning networks...");
+
+  int n = WiFi.scanNetworks();
+
+  if (n == 0) {
+    Serial.println("No networks found.");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks found:");
+    for (int i = 0; i < n; i++) {
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.println(" dBm)");
+      delay(10);
+    }
+  }
+
+  Serial.println("");
+  delay(5000);
+}
