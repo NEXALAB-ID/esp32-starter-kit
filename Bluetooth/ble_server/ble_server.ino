2bb266f5e7d4d9505292a0b51c3d78579532be0e
Hanz <115916883+hanzlr@users.noreply.github.com>
2026-06-28 22:59:26 +0700

    Implement BLE server with characteristic and callbacks
    This code sets up a BLE server with a characteristic that can be read, written, and notified. It includes connection callbacks and advertises the service UUID.

--- Bluetooth/ble_server/ble_server.ino
+++ Bluetooth/ble_server/ble_server.ino
@@ -1,1 +1,57 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+
+class ServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+    Serial.println("Device connected.");
+  }
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+    Serial.println("Device disconnected.");
+    pServer->startAdvertising();
+  }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  BLEDevice::init("NEXALAB-ESP32");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new ServerCallbacks());
+
+  BLEService* pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ |
+    BLECharacteristic::PROPERTY_WRITE |
+    BLECharacteristic::PROPERTY_NOTIFY
+  );
+
+  pCharacteristic->addDescriptor(new BLE2902());
+  pService->start();
+
+  BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->start();
+
+  Serial.println("BLE Server ready. Waiting for connections...");
+}
+
+void loop() {
+  if (deviceConnected) {
+    String value = "Hello from NEXALAB!";
+    pCharacteristic->setValue(value.c_str());
+    pCharacteristic->notify();
+    delay(2000);
+  }
+}
